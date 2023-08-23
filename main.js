6c30127915eea6c34d9c0a0c4593d9e583292d4c
Richard Olpindo <djricky088@yahoo.com>
2023-08-22 18:44:58 -1000

    Revised JS file based on feedback.

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ let favePokedex;
 let mainPokedex;
 let allTypes = [];
 
-let pokemonNumber
-let pokemonNameLC
-let pokemonName
-let pokemonImg
-let pokeSprite
-let primaryType
-let primaryTypeClass
+let pokemonNumber;
+let pokemonNameLC;
+let pokemonName;
+let pokemonImg;
+let pokeSprite;
+let primaryType;
+let primaryTypeClass;
+
+let sortArrs;
+let sortDirection = 1;
 
 function fetchAllPokemon() {
   let promises = []
@@ -28,92 +31,93 @@ function fetchAllPokemon() {
 
 fetchAllPokemon().then((response) => {
     return Promise.all(response.map((res) => res.json()))
-  }).then(data => {
-    for (let n = 0; n < 151; n++) {
-      const pD = data[n];
-      pokemonNumber = String(pD.id).padStart(3, '0');
-      pokemonNameLC = pD.name;
-      pokemonName = pokemonNameLC.charAt(0).toUpperCase() + pokemonNameLC.slice(1);
-      pokemonImg = pD.sprites.other['official-artwork'].front_default;
-      pokeSprite = pD.sprites.versions['generation-iii']['firered-leafgreen'].front_default;
-      primaryTypeClass = pD.types[0].type.name;
-      allTypes.push(primaryType);
-      primaryType = primaryTypeClass.toUpperCase();
-      if (pD.types.length > 1) {
-        secondaryType = pD.types[1].type.name.toUpperCase();
-        allTypes.push(secondaryType);
-      }
-      pokedex.innerHTML += `
-      <div id="${pokemonNumber}" class="pokebox ${primaryTypeClass} main">
-        <div class="pokemonPic">
-          <img class="pokeArtwork" src="${pokemonImg}" alt="${pokemonName}">
-          <i class="fa-regular fa-star"></i>
-          <img class="pokeSprite" src="${pokeSprite}" alt="${pokemonName}">
-        </div> 
-        <h2>${pokemonNumber}</h2>
-        <h4>${pokemonName}</h4>
-        <div class="typing">
-          <h5>${primaryType}</h5>
-        </div>
-      </div>`;
-      const currentPokemon = document.getElementById(`${pokemonNumber}`);
-      if (pD.types.length > 1) {
-        const currentPokemonType = currentPokemon.querySelector('.typing')
-        currentPokemonType.innerHTML += `<h5>${secondaryType}</h5>`;
-      };
-    }
-  })
-  .then(() => {
-    sortByPokedexNumber('asc');
-    for (const elm of selectCollection) {
-      elm.addEventListener('click', function () {
-        setActive(elm)
-      })
-    }
-  })
-  .then(() => {
-    starIcon = document.querySelectorAll('.fa-star');
-    for (const elm of starIcon) {
-      elm.addEventListener('click', function () {
-        const value = elm.parentElement.parentElement.parentElement.id === 'mainPokemon' ?
-          'toFaves' : 'toMain';
-        console.log(elm.parentElement.parentElement.id);
-        updateFavStatus(elm.parentElement.parentElement.id, value);
-      })
-    }
-  })
-  .then(() => {
-    function faveStorage(elm) {
-      const pokemon = document.querySelectorAll('.pokebox')
-      for (elm of pokemon) {
-        if (localStorage.getItem('favoritePokemon') &&
-          localStorage.getItem('favoritePokemon').split(',').includes(elm.id)) {
-          console.log(elm.id);
-          updateFavStatus(elm.id, 'toFaves');
-        }
-      }
-    }
-    faveStorage();
-  })
-  .then(() => {
-    calculateType();
-  })
+  }).then((data) =>
+    buildUI(data))
   .catch((err) => {
     console.log('Pokemon not found', err);
   })
 
-const setActive = (elm) => {
-  console.log(elm);
-  if (elm.innerHTML === 'Main Collection') {
-    console.log(faveContainer);
-    faveContainer.classList.remove('active');
-    mainContainer.classList.add('active');
-  } else {
-    console.log(mainContainer);
-    console.log(faveContainer);
-    mainContainer.classList.remove('active');
-    faveContainer.classList.add('active');
+function buildUI(data) {
+  createCards(data);
+  sortPokemon();
+  setCollectionListeners();
+  setFavListeners();
+  faveStorage();
+  calculateType();
+  setSortListener();
+}
+
+function createCards(data) {
+  for (let n = 0; n < 151; n++) {
+    const pD = data[n];
+    pokemonNumber = String(pD.id).padStart(3, '0');
+    pokemonNameLC = pD.name;
+    pokemonName = pokemonNameLC.charAt(0).toUpperCase() + pokemonNameLC.slice(1);
+    pokemonImg = pD.sprites.other['official-artwork'].front_default;
+    pokeSprite = pD.sprites.versions['generation-iii']['firered-leafgreen'].front_default;
+    primaryTypeClass = pD.types[0].type.name;
+    allTypes.push(primaryType);
+    primaryType = primaryTypeClass.toUpperCase();
+    if (pD.types.length > 1) {
+      secondaryType = pD.types[1].type.name.toUpperCase();
+      allTypes.push(secondaryType);
+    }
+    pokedex.innerHTML += `
+    <div id="${pokemonNumber}" class="pokebox ${primaryTypeClass} main">
+      <div class="pokemonPic">
+        <img class="pokeArtwork" src="${pokemonImg}" alt="${pokemonName}">
+        <i class="fa-regular fa-star"></i>
+        <img class="pokeSprite" src="${pokeSprite}" alt="${pokemonName}">
+      </div> 
+      <h2>${pokemonNumber}</h2>
+      <h4>${pokemonName}</h4>
+      <div class="typing">
+        <h5>${primaryType}</h5>
+      </div>
+    </div>`;
+    const currentPokemon = document.getElementById(`${pokemonNumber}`);
+    if (pD.types.length > 1) {
+      const currentPokemonType = currentPokemon.querySelector('.typing')
+      currentPokemonType.innerHTML += `<h5>${secondaryType}</h5>`;
+    };
+  };
+};
+
+function setCollectionListeners() {
+  for (const elm of selectCollection) {
+    elm.addEventListener('click', function () {
+      setActive(elm)
+      sortPokemon();
+    })
   }
+}
+
+function setFavListeners() {
+  const starIcon = document.querySelectorAll('.fa-star');
+  for (const elm of starIcon) {
+    elm.addEventListener('click', function () {
+      const value = elm.parentElement.parentElement.parentElement.id === 'mainPokemon' ?
+        'toFaves' : 'toMain';
+      updateFavStatus(elm.parentElement.parentElement.id, value);
+    })
+  }
+}
+
+function faveStorage(elm) {
+  const pokemon = document.querySelectorAll('.pokebox')
+  for (elm of pokemon) {
+    if (localStorage.getItem('favoritePokemon') &&
+      localStorage.getItem('favoritePokemon').split(',').includes(elm.id)) {
+      updateFavStatus(elm.id, 'toFaves');
+    }
+  }
+}
+
+const setActive = (elm) => {
+  const containers = [faveContainer, mainContainer]
+  const params = elm.innerHTML === "Main Collection" ? containers : containers.reverse();
+  params[0].classList.remove('active');
+  params[1].classList.add('active');
 }
 
 
@@ -124,28 +128,29 @@ const declareArrs = () => {
   const faveItems = document.querySelectorAll('.faves');
   const mainArr = Array.from(mainItems);
   const faveArr = Array.from(faveItems);
-  containerArr = [mainPokemon, favePokemon];
-  itemArr = [mainArr, faveArr];
+  const containerArr = [mainPokemon, favePokemon];
+  const itemArr = [mainArr, faveArr];
+  return sortArrs = [itemArr, containerArr];
 }
 
 const sortByPokedexNumber = (direction) => {
   declareArrs();
-  for (i = 0; i < itemArr.length; i++) {
-    itemArr[i].sort((a, b) => {
+  for (i = 0; i < sortArrs[0].length; i++) {
+    sortArrs[0][i].sort((a, b) => {
       const numA = parseInt(a.querySelector('h2').innerHTML);
       const numB = parseInt(b.querySelector('h2').innerHTML);
       return direction === 'asc' ? numA - numB : numB - numA;
     })
-    itemArr[i].forEach((item) => {
-      containerArr[i].append(item);
+    sortArrs[0][i].forEach((item) => {
+      sortArrs[1][i].append(item);
     });
   }
 }
 
 const sortByPokemonName = (direction) => {
   declareArrs();
-  for (i = 0; i < itemArr.length; i++) {
-    itemArr[i].sort((a, b) => {
+  for (i = 0; i < sortArrs[0].length; i++) {
+    sortArrs[0][i].sort((a, b) => {
       const nameA = a.querySelector('h4').innerHTML;
       const nameB = b.querySelector('h4').innerHTML;
       if (nameA < nameB) {
@@ -156,25 +161,40 @@ const sortByPokemonName = (direction) => {
         return 0;
       }
     })
-    itemArr[i].forEach((item) => {
-      containerArr[i].append(item);
+    sortArrs[0][i].forEach((item) => {
+      sortArrs[1][i].append(item);
     });
   }
 }
 
-const sortPokemon = document.getElementById('sortPokemonBy');
+function setSortListener () {
+const sortPokemonValue = document.getElementById('sortPokemonBy');
 
-sortPokemon.addEventListener('click', function () {
+sortPokemonValue.addEventListener('change', function () {
   if (this.value === 'Number (Ascending)') {
-    sortByPokedexNumber('asc');
+    sortDirection = 1;
   } else if (this.value === 'Number (Descending)') {
-    sortByPokedexNumber('desc');
+    sortDirection = 2;
   } else if (this.value === 'Name (Ascending)') {
-    sortByPokemonName('asc');
+    sortDirection = 3;
   } else if (this.value === 'Name (Descending)') {
-    sortByPokemonName('desc');
+    sortDirection = 4;
   }
-});
+  sortPokemon();
+})
+}
+
+function sortPokemon () {
+  if (sortDirection === 1) {
+    sortByPokedexNumber('asc');
+  } else if (sortDirection === 2) {
+    sortByPokedexNumber('desc');
+  } else if (sortDirection === 3) {
+    sortByPokemonName ('asc');
+  } else if (sortDirection === 4) {
+    sortByPokemonName ('desc');
+  }
+}
 
 const updateFavStatus = (id, direction) => {
   const selectedID = document.getElementById(id);
@@ -182,8 +202,7 @@ const updateFavStatus = (id, direction) => {
   const starArr = ['fa-regular', 'fa-solid'];
   const containerArr = [favePokemon, mainPokemon];
   const classArr = ['faves', 'main'];
-  const params = direction === 'toFaves' ? 
-  [containerArr, starArr, classArr, addId(id)] : [containerArr.reverse(), starArr.reverse(), classArr.reverse(), deleteId(id)];
+  const params = direction === 'toFaves' ? [containerArr, starArr, classArr, addId(id)] : [containerArr.reverse(), starArr.reverse(), classArr.reverse(), deleteId(id)];
   params[0][1].removeChild(selectedID);
   params[0][0].appendChild(selectedID);
   selectedID.classList.remove(params[2][1]);
@@ -214,7 +233,7 @@ function calculateType() {
     'steel', 'bug', 'dark', 'fairy', 'dragon',
     'ghost'
   ];
-  for (specType of types) {
+  for (let specType of types) {
     let a = specType.toUpperCase();
     let b = 0;
     for (types of allTypes) {
